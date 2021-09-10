50d323081252d510fe4ed7d51028ac9cc89ef533
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2021-09-10 00:10:02 -0400

    new change file

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
 
+from . import models
 from . import res_config_settings
-from . import ir_ui_view
-from . import base_language_install
-from . import ir_module_module
-from . import web_environment_ribbon_backend
-# from . import ir_ui_menu
