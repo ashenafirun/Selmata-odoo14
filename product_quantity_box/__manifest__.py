2b364cd2e794d4b44126bf2df553c5440e07187f
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2021-09-22 20:21:01 -0400

    Add files via upload

---
+++ product_quantity_box/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Atharva System. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Product Quantity Box',
+    'category': 'Sales',
+    'summary': """Add quantity box in website shop page.Customer quick add items to shopping cart
+    Qty box,
+    Quick Add to Cart,
+    Add To Cart With Quantity,
+    Quick Add To Cart,
+    Product qty box,
+    """,
+    'version': '1.0',
+    'license': 'OPL-1',
+    'author': 'Atharva System',
+    'website': "https://www.atharvasystem.com",
+    'support': 'support@atharvasystem.com',
+    'description': 'Add quantity box in website shop page',
+    'depends': ['website_sale'],
+    'data': [
+        'views/template.xml',
+    ],
+    'images': ['static/description/qty_box.jpg'],
+    'price': 25.00,
+    'currency': 'EUR',
+    'installable': True,
+    'application': True,
+
+}
